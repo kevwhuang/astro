41f8b8166f45be841c9621c457cd64ee6310bbb8
Kevin Huang <kevwhuang@gmail.com>
2025-01-19 03:11:03 -0600

    Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 const astro = defineConfig({
+    integrations: [sitemap({ lastmod: new Date() })],
+    site: 'https://k-astro.netlify.app',
 });
 
 export default astro;
