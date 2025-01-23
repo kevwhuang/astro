eeaf6cf8673082c16c85dfaac7ee7dc5f4627831
Kevin Huang <kevwhuang@gmail.com>
2025-01-23 08:08:39 -0600

    Error pages

--- netlify/edge-functions/geo.tsx
+++ netlify/edge-functions/geo.tsx
@@ -9,13 +9,39 @@ async function handler(res: Response, context: Context): Promise<Response> {
 
     const { geo } = context;
 
+    const style = {
+        a: {
+            color: '#003049',
+            textDecoration: 'none',
+        },
+        body: {
+            OverflowX: 'hidden',
+            background: '#fdf0d5',
+            fontFamily: 'sf pro display,helvetica neue,sans-serif',
+            fontSize: '25.89px',
+            lineHeight: 1.5,
+            margin: '25.89px',
+        },
+        p: {
+            marginBlock: 0,
+        },
+    };
+
     const jsx = (
         <html lang="en-us">
-            <body>
+            <head>
                 <title>Geo</title>
-                <a href="/">Home</a>
-                <p>{geo.latitude}, {geo.longitude}</p>
-                <p>{geo.city}, {geo.subdivision?.name}, {geo.country?.name}</p>
+                <meta charSet="utf-8" />
+                <meta content="Kevin Huang" name="author" />
+                <meta content="ie=edge" http-equiv="x-ua-compatible" />
+                <meta content="noindex" name="robots" />
+                <meta content="width=device-width,initial-scale=1" name="viewport" />
+            </head>
+
+            <body style={style.body}>
+                <a href="/" style={style.a}>Home</a>
+                <p style={style.p}>{geo.latitude}, {geo.longitude}</p>
+                <p style={style.p}>{geo.city}, {geo.subdivision?.name}, {geo.country?.name}</p>
             </body>
         </html>
     );
