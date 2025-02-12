e99c17055df051bc8ab455efa60b01fd4131f011
Kevin Huang <kevwhuang@gmail.com>
2025-02-11 23:05:13 -0600

    Shiki

---
+++ src/tests/config.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test';
+
+import { exists } from '@/utils/_all';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/config');
+});
+
+test('page', async ({ page }) => {
+    await expect(page).toHaveTitle('Config');
+    await exists(page, 'navigation');
+    await expect(page.locator('css=main>section')).toHaveCount(1);
+    await exists(page, 'contentinfo');
+});
+
+test('section 1', async ({ page }) => {
+    await expect(page.locator('css=code')).toBeVisible();
+});
